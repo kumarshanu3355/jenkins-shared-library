08985a515a9feb237d68cb2ee3accc7fc3579ed6
Kumar Shanu <157588356+kumarshanu3355@users.noreply.github.com>
2026-09-07 22:28:35 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String imgname, String imgtag,String dockeruserid){
-  withCredentials([usernamePassword(credentialId: "dockercred", usernameVariable: "dockeruser", passwordVariable: "dockerpass")]){
+  withCredentials([usernamePassword(credentialsId: "dockercred", usernameVariable: "dockeruser", passwordVariable: "dockerpass")]){
     sh "echo ${dockerpass} | docker login -u ${dockeruser} --password-stdin"
     sh "docker tag ${imgname}:${imgtag} ${dockeruserid}/${imgname}:${imgtag}"
     sh "docker push ${dockeruserid}/${imgname}:${imgtag}"
